ec4bcacdc58b47d3b6cad759fe56a2da94c58662
Vitor Silva Reis <vitorejuvian@hotmail.com>
2022-11-01 19:54:36 -0300

    Creating main.py and myGraph.ncol Configuring gitignore

---
+++ main.py
@@ -0,0 +1,46 @@
+from igraph import *
+import matplotlib.pyplot as plt
+
+def main():
+    graph = Graph.Read_Ncol("myGraph.ncol", directed=False)
+    graph.es['color'] = None
+
+    colors = ['red', 'green', 'blue', 'black', 'grey', 'yellow', 'purple']
+    loop = 1
+
+    while loop:
+        group = []
+        for edge in graph.es:
+            append = 1
+            if edge['color'] is None:
+                for edge2 in group:
+                    if edge2 != edge and edge.source == edge2.source or edge.source == edge2.target or\
+                            edge.target == edge2.source or edge.target == edge2.target:
+                        append = 0
+                if append:
+                    group.append(edge)
+
+        for edge in group:
+            if len(colors) >= 1:
+                edge['color'] = colors[0]
+            else:
+                print('ERRO: Cores insuficientes')
+
+        if len(colors) >= 1:
+            colors.pop(0)
+        loop = 0
+
+        for e in graph.es:          # If there is any edge without color, run again
+            if e['color'] is None:
+                loop = 1
+
+    for e in graph.es:
+        print(f'Edge[{graph.vs[e.source]["name"]}, {graph.vs[e.target]["name"]}] = {e["color"]}')
+
+    plot(graph, target=plt.axes())
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
